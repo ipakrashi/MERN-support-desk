03e730666e4442649278d3bf4eaadef2d274811d
ipakrashi <indranil.pakrashi@gmail.com>
2026-07-03 00:32:46 +0530

    123. Connect To The Database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const dns = require('dns')
 dns.setServers(['8.8.8.8', '8.8.4.4'])
 const express = require('express')
+const colors = require('colors')
 const router = require('./routes/userRoutes')
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const dotenv = require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 8000
+
+// Connect To The Database
+connectDB()
 
 // Middleware
 app.use(express.urlencoded({ extended: false }))
@@ -18,5 +23,5 @@ app.use(errorHandler)
 
 // Server Start
 app.listen(PORT, () => {
-    console.log(`Server Started On Port : ${PORT}`)
+    console.log(`Server Started On Port : ${PORT}`.green.underline.bold)
 })
