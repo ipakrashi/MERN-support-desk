4b124f4b3302827295a4cdc287f7b48b36043ccf
ipakrashi <indranil.pakrashi@gmail.com>
2026-07-03 00:06:06 +0530

    122. Error & Exception Handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,26 @@
-const registerUser = (req, res) => {
-    res.send('Register Route')
-}
+const asyncHandler = require('express-async-handler')
 
-const loginUser = (req, res) => {
-    res.send('Login Route')
-}
+// REGISTER AN USER
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please include all fields')
+    }
+    return res.status(200).json({ message: 'Register Route' })
+})
+
+// LOGIN AN USER
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        res.status(400)
+        throw new Error('Please include all fields')
+    }
+    return res.status(200).json({ message: 'Login Route' })
+})
 
 module.exports = { registerUser, loginUser }
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,19 @@ const dns = require('dns')
 dns.setServers(['8.8.8.8', '8.8.4.4'])
 const express = require('express')
 const router = require('./routes/userRoutes')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const dotenv = require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 8000
 
 // Middleware
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 // Routes
 app.use('/api/users', router)
+
+app.use(errorHandler)
 
 // Server Start
 app.listen(PORT, () => {
