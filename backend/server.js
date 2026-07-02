afb17c28543ac86a56460ad7cb7591042dfa2300
ipakrashi <indranil.pakrashi@gmail.com>
2026-07-02 23:03:24 +0530

    120. Basic Express Server Setup

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 const dns = require('dns')
 dns.setServers(['8.8.8.8', '8.8.4.4'])
 const express = require('express')
+const dotenv = require('dotenv').config()
 const app = express()
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 // Middleware
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 // Routes
+app.get('/api/users', (req, res) => {
+    res.status(200).json({ message: 'Welcome to Support Desk API' })
+})
 
 // Server Start
 app.listen(PORT, () => {
