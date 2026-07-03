6735960205b92b418c0da55e66f6bba4296142b4
ipakrashi <indranil.pakrashi@gmail.com>
2026-07-03 22:50:33 +0530

    129. Home, Login & Register UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,8 @@ import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Header from './components/Header'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
     return (
@@ -23,6 +25,7 @@ function App() {
                     </Routes>
                 </div>
             </Router>
+            <ToastContainer />
         </>
     )
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
-import logo from '../../public/helpdesk.png'
+import logo from '/helpdesk.png'
 function Header() {
     return (
         <header className='header'>
