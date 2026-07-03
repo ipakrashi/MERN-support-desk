305c9498954c3bfda2f49176e04989af12717fd8
ipakrashi <indranil.pakrashi@gmail.com>
2026-07-03 11:27:23 +0530

    124. Register User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require('express-async-handler')
+const userModel = require('../models/userModel')
+const bcrypt = require('bcryptjs')
 
 // REGISTER AN USER
 
@@ -9,7 +11,33 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please include all fields')
     }
-    return res.status(200).json({ message: 'Register Route' })
+
+    // Find if the User exists
+    const userExists = await userModel.findOne({ email })
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    // Hash Password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create User
+    const user = await userModel.create({
+        name,
+        email,
+        password: hashedPassword,
+    })
+
+    if (user) {
+        return res
+            .status(201)
+            .json({ _id: user._id, name: user.name, email: user.email })
+    } else {
+        res.status(400)
+        throw new Error('User already exists')
+    }
 })
 
 // LOGIN AN USER
